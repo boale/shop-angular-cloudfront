f1faa0f0b79f5126a8b1a66ce0932c1a6809fcea
Oleksandr Bondarenko <oleksandr_bondarenko2@epam.com>
2022-06-22 15:06:24 +0300

    feat: cicd lab

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://h8034v6msh.execute-api.us-east-1.amazonaws.com/prod',
+    product: 'https://05dadzcc7i.execute-api.us-east-1.amazonaws.com/prod',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
